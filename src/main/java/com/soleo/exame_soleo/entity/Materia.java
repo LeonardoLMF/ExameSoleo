c13103f03a8b8d67ffd56fdbfe2072701daadcc7
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-03-11 11:39:07 -0300

    feat: Criação da entidade Materia

--- src/main/java/com/soleo/exame_soleo/entity/Materia.java
+++ src/main/java/com/soleo/exame_soleo/entity/Materia.java
@@ -1,14 +1,17 @@
 package com.soleo.exame_soleo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "materias")
 @Getter
 @Setter
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Materia {
 
     @Id
