3773462ab7c4cbe025601bfc076faaa0d0471399
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-03-11 09:38:17 -0300

    start project ExameSoleo

---
+++ src/main/java/com/soleo/exame_soleo/ExameSoleoApplication.java
@@ -0,0 +1,13 @@
+package com.soleo.exame_soleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameSoleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameSoleoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soleo/exame_soleo/ExameSoleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soleo.exame_soleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameSoleoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
