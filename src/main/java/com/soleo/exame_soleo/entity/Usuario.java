50f051fc8935af5af97a9fc4b094f8b2e10a90d9
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-03-11 11:22:47 -0300

    feat: Criação da entidade Usuario, e Enum Role

---
+++ src/main/java/com/soleo/exame_soleo/entity/Usuario.java
@@ -0,0 +1,33 @@
+package com.soleo.exame_soleo.entity;
+
+
+import com.soleo.exame_soleo.enums.Role;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/com/soleo/exame_soleo/enums/Role.java
@@ -0,0 +1,5 @@
+package com.soleo.exame_soleo.enums;
+
+public enum Role {
+    ALUNO,PROFESSOR;
+}
